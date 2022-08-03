5beed33bd7bb15a5d1050e80d4caa603f9c25021
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-08-02 19:47:44 -0700

    install bootstrap

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export function Header() {
+    return(
+        <div>
+            Cupcake Makes
+            <button type='button' className='btn btn-success'>Click Me</button>
+        </div>
+    )
+}
