e0d211a38a6e5f6798ab34987cd5d25fec6070c8
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-08-04 22:45:24 -0700

    read data from json file

--- src/components/ProductCardDisplay.js
+++ src/components/ProductCardDisplay.js
@@ -1,13 +1,14 @@
 import products from '../data/data.json'
 
-
 export function ProductCardDisplay() {
+    const product = products.products
+    console.log(product[0].product_img);
     return(
         <div className="card">
-            <img src={require(products[0].product_img)} className="card-img-top" alt="..."></img>
+            <img src={require(`${product[0].product_img}`)} className="card-img-top" alt="..."></img>
             <div className="card-body">
-                <h2>{products[0].product_name}</h2>
-                <p className="card-text">{products[0].product_desc}</p>
+                <h2>{product[0].product_name}</h2>
+                <p className="card-text">{product[0].product_desc}</p>
             </div>
         </div>
     )
