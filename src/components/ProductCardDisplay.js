4e496edab69e1903409ef9b760276e9287ca3637
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-08-05 01:31:06 -0700

    Update ProductCardDisplay.js

--- src/components/ProductCardDisplay.js
+++ src/components/ProductCardDisplay.js
@@ -5,7 +5,7 @@ export function ProductCardDisplay() {
     console.log(product[0].product_img);
     return(
         <div className="card">
-            <img src={require(`${product[0].product_img}`)} className="card-img-top" alt="..."></img>
+            <img src={require(`../imgs/oreo.jpeg`)} className="card-img-top" alt="..."></img>
             <div className="card-body">
                 <h2>{product[0].product_name}</h2>
                 <p className="card-text">{product[0].product_desc}</p>
