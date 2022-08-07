2af35845c38100059e9b61cccf937d35628f778d
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-08-06 18:55:10 -0700

    display based on data file

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,19 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import { ProductCardDisplay } from "./ProductCardDisplay";
+import products from '../data/data.json'
+
 
 export function Body() {
+    const product = products.products
+    let key;
+    const cards = product.map((card) => {
+        key++
+        return <ProductCardDisplay 
+            img={card.product_img}
+            name={card.product_name}
+            key={key}
+        />
+    })
     return(
         <div className="text-center">
             <h1 className="mt-3 fs-1">
@@ -10,19 +22,8 @@ export function Body() {
             <p className="text-muted">*All cupcakes start at $15</p>
             <Container>
             <hr className="mb-3"></hr>
-                <Row className="mb-5 mx-auto" xs={2} md={4}>
-                    <Col className="mb-4">
-                        <ProductCardDisplay />
-                    </Col>                    
-                    <Col>
-                        <ProductCardDisplay />
-                    </Col>  
-                    <Col>
-                        <ProductCardDisplay />
-                    </Col>
-                    <Col>
-                        <ProductCardDisplay />
-                    </Col>
+                <Row className="mb-5 m-auto" xs={2} md={4}>
+                    {cards}
                 </Row>
             </Container>
             {/* Move this to the end */}
--- src/components/ProductCardDisplay.js
+++ src/components/ProductCardDisplay.js
@@ -1,16 +1,18 @@
-import { Card } from 'react-bootstrap';
+import { Card, Col } from 'react-bootstrap';
 import products from '../data/data.json'
 
-export function ProductCardDisplay() {
+export function ProductCardDisplay(props) {
     const product = products.products
     console.log(product[0].product_img);
     return(
-        <Card style={{ width: '10rem' }}>
-            <Card.Img variant='top' src={require(`../imgs/flower.jpeg`)} />
-            <Card.Body className='p-1 pt-2'>
-                <Card.Title className='fs-6'>{product[0].product_name}</Card.Title>
-            </Card.Body>
-        </Card>
+        <Col className='mb-4'>
+            <Card style={{ width: '10rem' }}>
+                <Card.Img variant='top' src={require(`../imgs/${props.img}`)} width={150} height={150} />
+                <Card.Body className='p-1 pt-2'>
+                    <Card.Title className='fs-6'>{props.name}</Card.Title>
+                </Card.Body>
+            </Card>
+        </Col>
         // <div className="card">
         //     <img src={require(`../imgs/${product[0].product_img}`)} className="card-img-top" alt="..."></img>
         //     <div className="card-body">
