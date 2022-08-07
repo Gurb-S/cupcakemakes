86a41bad3bf92b71cb437ae2ec4d8e5dd35d43fd
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-08-07 00:34:57 -0700

    add cake imgs

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,24 @@
 import { Container, Row } from "react-bootstrap";
 import { ProductCard } from "./ProductCard";
 import cupcake from '../data/cupcake.json'
+import cake from '../data/cake.json'
 
 
 export function Body() {
-    const product = cupcake.products
+    const cupcake_product = cupcake.products
+    const cake_product = cake.products
+    console.log(cake_product)
     let key;
-    const cupcakecards = product.map((card) => {
+    // const cupcakeCards = cupcake_product.map((card) => {
+    //     key++
+    //     return <ProductCard 
+    //         img={card.product_img}
+    //         name={card.product_name}
+    //         key={key}
+    //     />
+    // })
+
+    const cakeCards = cake_product.map((card) => {
         key++
         return <ProductCard 
             img={card.product_img}
@@ -20,13 +32,19 @@ export function Body() {
                 CUPCAKES
             </h1>
             <p className="text-muted">*All cupcakes start at $15</p>
+            {/* <Container>
+            <hr className="mb-3"></hr>
+                <Row className="mb-2 m-auto" xs={2} md={4}>
+                    {cupcakeCards}
+                </Row>
+            </Container> */}
+            <a  href="https://www.google.com" className="text-center fs-3">View More</a>
             <Container>
             <hr className="mb-3"></hr>
                 <Row className="mb-2 m-auto" xs={2} md={4}>
-                    {cupcakecards}
+                    {cakeCards}
                 </Row>
             </Container>
-            <a  href="https://www.google.com" className="text-center fs-3">View More</a>
         </div>
     )
 }
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,7 +4,7 @@ export function ProductCard(props) {
     return(
         <Col className='mb-4'>
             <Card style={{ width: '10rem' }}>
-                <Card.Img variant='top' src={require(`../imgs/${props.img}`)} width={150} height={150} />
+                <Card.Img variant='top' src={require(`../imgs/cake/${props.img}`)} width={150} height={150} />
                 <Card.Body className='p-1 pt-2'>
                     <Card.Title className='fs-6'>{props.name}</Card.Title>
                 </Card.Body>
