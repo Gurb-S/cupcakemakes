9265405f5e01c48138a554487e276478da04baba
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-08-22 15:34:21 -0700

    updates price of item based on how many selected

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Carousel, Container, Form, Button, Row, Col } from 'react-bootstrap';
 import { useParams } from 'react-router-dom'
 import products from '../data/data.json'
@@ -6,19 +6,19 @@ import SiteContext from '../context/Context';
 import { toast, ToastContainer } from 'react-toastify';
 
 export function ProductPage() {
+
+    // stores the value of the id param in the url
     const { id } = useParams();
     // gets the cupcake who's id matches the number in the url
     const cupcake = products.cupcakes[id];
-
     //import from context api
     const { setCookie } = useContext(SiteContext);
-
-    // replaces spaces in cupcake name with an underscore "_"
+    // name of cupcake selected
     let cupcakeName = cupcake.product_name;
-
+    // price of the cupcake selected
+    let cupcakePrice = cupcake.product_price;
     // keeps track of counter on page
     const [ count, setCount ] = useState(1);
-
     // when submitted creates a cookie with the name of the cupcake and number of cupcakes
     const handleSubmit = (e) =>{
         e.preventDefault();
@@ -46,13 +46,13 @@ export function ProductPage() {
                 </Carousel.Item>
             </Carousel>
             <div className='text-start'>
-                <h1 className='mt-3 fs-4'>$15 per dozen</h1>
+                <h1 className='mt-3 fs-4'>${`${cupcakePrice}` * count}</h1>
                 <p>{cupcake.product_desc}</p>
             </div>      
                 <Form onSubmit={handleSubmit}>
                     <Container className='mx-auto' style={{ width: '200px' }}>
                         <Row xs='auto'>
-                            <Col className='ms-3 minus' onClick={() => count >0 ? setCount(count - 1)  : null}> 
+                            <Col className='ms-3 minus' onClick={() =>  count > 0 ? setCount(count - 1) : setCount(0) }> 
                                 <svg style={{ cursor: 'pointer' }} id="minus" xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" className="bi bi-patch-minus-fill" viewBox="0 0 16 16">
                                 <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zM6 7.5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1 0-1z"/>
                                 </svg>
@@ -60,7 +60,7 @@ export function ProductPage() {
                             <Col>
                                 <p className='mt-1'>{count}</p>
                             </Col>
-                            <Col onClick={() => setCount(count + 1)}>
+                            <Col onClick={() => setCount(count + 1) }>
                                 <svg style={{ cursor: 'pointer' }}  xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" className="bi bi-patch-plus-fill" viewBox="0 0 16 16">
                                 <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zM8.5 6v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 1 0z"/>
                                 </svg>
