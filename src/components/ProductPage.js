d1e21607e5d109a4a9d6e5545b0958f1d9593189
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-08-22 19:40:59 -0700

    remove useEffect hook

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Carousel, Container, Form, Button, Row, Col } from 'react-bootstrap';
 import { useParams } from 'react-router-dom'
 import products from '../data/data.json'
