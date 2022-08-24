b7fddc44e62e89d0e4d57ecc3119e7dd50259b55
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-08-24 00:24:14 -0700

    add btns to cart cards

--- src/components/CartCards.js
+++ src/components/CartCards.js
@@ -1,75 +1,63 @@
-import React, { useContext } from "react"
+import React, { useState, useEffect, useContext } from "react"
+import { Col, Row } from "react-bootstrap"
+import Cookies from "js-cookie";
 import SiteContext from "../context/Context"
-import products from '../data/data.json'
-import { Card } from "react-bootstrap"
 
-export function CartCards() {
+export function CartCards(props) {
 
     // TODO: Clear cart ability needs to be added
-    // TODO: crashes if cart is visited when empty
+    // // TODO: crashes if cart is visited when empty
     // TODO: issue with items not being displayed, replicated on phone
     // * Error: object is not valid as React child
     // * Solution: seems like an issue with the view app on network. Seems like this isn't an issue in production
 
-    const { allCookies } = useContext(SiteContext)
+    //let test = (props.amount * 0)
+    //console.log(props.amount)
+    const [ count, setCount ] = useState(parseInt(props.amount))
 
-    // console.log(allCookies)
+    //import from context api
+    const { setCookie } = useContext(SiteContext);
 
-    // if(allCookies){
-    //     console.log('NO COOKIESSSS')
-    //     console.log(Object.keys(allCookies).length)
-    // }
+    useEffect(() =>{
+        setCookie(props.name,count)
+    },[count])
 
-    // if(Object.keys(allCookies).length <= 0){
-    //     console.log('THERE ARE NO COOKIESSSSSSSSS')
-    // }
-
-    const cupcakesInCart =  Object.entries(allCookies)
-
-    const cupcakeDetails = cupcakesInCart.map(cupcake  => {
-        let productImgs;
-        let productPrices;
-        for(let j = 0; j < products.cupcakes.length;j++){
-            if(products.cupcakes[j].product_name === cupcake[0]){
-                productImgs = products.cupcakes[j].product_img
-                productPrices = products.cupcakes[j].product_price
-
-            }
-        }
-        return {
-            productName : cupcake[0],
-            productCount: cupcake[1],
-            productImg: productImgs,
-            productPrice: productPrices
-        }
-    })
-
-   // * move the above contents back to the checkout page and page in the data via props to this page
-   // * and use it to create the cards for each item and can easier check for product count being 0
-   // * similar relationship between body and product page
-
-    const itemArray = cupcakeDetails.map(item => {
-        return (
-            (item.productCount > 0 ?
-            <div className="d-flex border border-secondary rounded m-3">
+    const removeItem = () => {
+        const elem = document.getElementById("cartCard");
+        elem.remove();
+        Cookies.remove(`${props.name}`)
+        console.log(elem)
+    }
+        
+    return(
+        <div className="d-flex border border-secondary rounded m-3" id="cartCard">
                 <div className="flex-shrink-0 my-2 ps-1">
-                    <img className="rounded" src={`${item.productImg}`} alt="img of cupcake" width={80} height={80}/>
+                    <img className="rounded" src={`${props.img}`} alt="img of cupcake" width={80} height={80}/>
                 </div>
                 <div className="flex-grow-1 ms-3">
-                    <h2 className="mt-1">{item.productName}</h2>
-                    <div className="d-inline-flex">
-                        <p>{item.productCount}</p>
-                        <p>${item.productCount * item.productPrice}</p>
+                    <h2 className="mt-1">{props.name}</h2>
+                    <div className="d-inline-flex align-items-center w-100 justify-content-around">
+                        <svg onClick={removeItem} style={{ cursor: 'pointer' }} xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
+                            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+                        </svg>
+                        <Row className="align-items-center justify-content-center">
+                            <Col onClick={() =>  count > 0 ? setCount(count - 1) : setCount(0) }> 
+                                <svg style={{ cursor: 'pointer' }} id="minus" xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" className="bi bi-patch-minus-fill" viewBox="0 0 16 16">
+                                <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zM6 7.5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1 0-1z"/>
+                                </svg>
+                            </Col>
+                            <Col>
+                                <p className='m-auto'>{count}</p>
+                            </Col>
+                            <Col onClick={() => setCount(count + 1) }>
+                                <svg style={{ cursor: 'pointer' }}  xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" className="bi bi-patch-plus-fill" viewBox="0 0 16 16">
+                                <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zM8.5 6v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 1 0z"/>
+                                </svg>
+                            </Col>
+                        </Row>
+                        <p className="mb-0 me-2 fs-3">${count * props.price}</p>
                     </div>
                 </div>
             </div>
-            : null)
-        )
-    })
-    
-    return(
-        <div>
-            {allCookies && itemArray.length > 0 ? itemArray : <h2 className="text-center mt-5">Cart is Empty</h2>}
-        </div>
     )
 }
--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,13 +1,47 @@
-import React from "react"
+import React, { useContext } from "react"
+import SiteContext from "../context/Context"
 import { CartCards } from "./CartCards"
-
+import products from '../data/data.json'
 
 export function Checkout() {
     
+    const { allCookies } = useContext(SiteContext)
+
+    const cupcakesInCart =  Object.entries(allCookies)
+
+    const cupcakeDetails = cupcakesInCart.map(cupcake  => {
+        let productImgs;
+        let productPrices;
+        for(let j = 0; j < products.cupcakes.length;j++){
+            if(products.cupcakes[j].product_name === cupcake[0]){
+                productImgs = products.cupcakes[j].product_img
+                productPrices = products.cupcakes[j].product_price
+
+            }
+        }
+        return {
+            productName : cupcake[0],
+            productCount: cupcake[1],
+            productImg: productImgs,
+            productPrice: productPrices
+        }
+    })
+
+    const cartArray = cupcakeDetails.map(item =>{
+        if(item.productCount > 0){
+            return <CartCards 
+                img={item.productImg}
+                name={item.productName}
+                amount={item.productCount}
+                price={item.productPrice}
+            />
+        }
+    })
+
     return (
         <div>
             <h1 className="text-center mt-3">Checkout</h1>
-            <CartCards />
+            {allCookies && cartArray.length > 0 ? cartArray : <h2 className="text-center mt-5">Cart is Empty</h2>}
         </div>
     )
 }
