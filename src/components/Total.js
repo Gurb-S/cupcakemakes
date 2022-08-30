26878e0c4ba6c14ca48bba2459e47da5048d4256
gurb <Organicmintz@gmail.com>
2022-08-29 17:47:27 -0700

    add cookies for total sum

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,12 +1,37 @@
-import React from "react"
+import React, { useContext, useEffect } from "react"
+import SiteContext from "../context/Context"
+import products from '../data/data.json'
 
-export function Total (){
+export function Total(props) {
+
+    // TODO: render automatically and change total everytime user adds more items while in cart
+
+    const { setCookie } = useContext(SiteContext);
+    console.log(props.cupcakes)
+
+    const totalPrice = props.cupcakes.map(cupcake => {
+        if(cupcake.productImg){
+            return parseInt(cupcake.productCount) * cupcake.productPrice
+        }
+        else{
+            return 0
+        }
+    })
+
+    let sum = totalPrice.reduce((a,b) => a + b,0);
+
+
+    useEffect(() => {
+        setCookie('Total', sum)
+    },[sum]);
+
+
     return(
         <div className="border border-secondary rounded m-3">
             <div className="text-bg-dark text-center fs-1">Order</div>
             <div className="mt-1 d-flex align-items-center justify-content-between">
                 <h1 className="ms-2">Total:</h1>
-                <h2 className="me-2">$200</h2>
+                <h2 className="me-2">${sum}</h2>
             </div>
         </div>
     )
