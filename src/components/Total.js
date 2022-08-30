09bf95074bf28b31d41985d0932f1ca2dfb9ba81
gurb <Organicmintz@gmail.com>
2022-08-30 14:58:18 -0700

    total calculation moved to contextapi

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,27 +1,17 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import SiteContext from "../context/Context"
 
 export function Total() {
 
     // TODO: render automatically and change total everytime user adds more items while in cart
 
-    const { setCookie, cupcakesInCart } = useContext(SiteContext);
-    console.log(cupcakesInCart)
-
-    const totalPrice = cupcakesInCart.map(cupcake => {
-        if(cupcake.productImg){
-            return parseInt(cupcake.productCount) * cupcake.productPrice
-        }
-        else{
-            return 0
-        }
-    })
-
-    let sum = totalPrice.reduce((a,b) => a + b,0);
-
+    const { total, getTotal } = useContext(SiteContext);
+    //console.log(cupcakesInCart)
     useEffect(() => {
-        setCookie('Total', sum)
-    },[sum]);
+        getTotal()
+    },[]);
+
+
 
 
     return(
@@ -29,7 +19,7 @@ export function Total() {
             <div className="text-bg-dark text-center fs-1">Order</div>
             <div className="mt-1 d-flex align-items-center justify-content-between">
                 <h1 className="ms-2">Total:</h1>
-                <h2 className="me-2">${sum}</h2>
+                <h2 className="me-2">${total}</h2>
             </div>
         </div>
     )
