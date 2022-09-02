41190313edbb5c4201054206f121ed0e850a7850
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-09-01 23:34:38 -0700

    add total counter

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import SiteContext from "../context/Context"
 export function Header() {
 
     const { numberOfItemsInCart } = useContext(SiteContext)
+    let cartNum = 0;
     // * use d-lg-none item when screen size large
     
     return(
