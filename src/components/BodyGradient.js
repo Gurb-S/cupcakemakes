9ef8de788ddf479e35da06e20aa3a01fc951eacf
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-09-03 23:44:43 -0700

    fix grammer errors

--- src/components/BodyGradient.js
+++ src/components/BodyGradient.js
@@ -3,7 +3,7 @@ import { Button } from "react-bootstrap"
 export function BodyGradient() {
     return(
         <div className="p-5 bg-secondary bg-gradient text-white">
-            <p>Welcome to Cupcake makes, we're a business based in Plumas Lake, CA</p>
+            <p>Welcome to Cupcake Makes, we're a business based in Plumas Lake, CA</p>
             <p>We offer both ordering predesigned cupcakes and making custom offers.</p>
             <div className="d-flex justify-content-center">
                 <Button variant="primary">Custom Order</Button>
