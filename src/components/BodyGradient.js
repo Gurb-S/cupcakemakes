03ddf5503d155e878cd12912c8b4e87a4fe1d4e3
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-09-04 23:29:22 -0700

    apply test body gradiant

--- src/components/BodyGradient.js
+++ src/components/BodyGradient.js
@@ -2,7 +2,7 @@ import { Button } from "react-bootstrap"
 
 export function BodyGradient() {
     return(
-        <div className="p-5 bg-secondary bg-gradient text-white">
+        <div className="p-5 bg-secondary bg-gradient text-white" id="test">
             <p>Welcome to Cupcake Makes, we're a business based in Plumas Lake, CA</p>
             <p>We offer both ordering predesigned cupcakes and making custom offers.</p>
             <div className="d-flex justify-content-center">
