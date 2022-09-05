3c8d87d4d5d8efab067e2d90449fc24257d9cb19
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-09-04 23:29:38 -0700

    remove unneeded imports

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext } from "react"
 import { Container, Nav, Navbar, Row, Col } from "react-bootstrap"
 import SiteContext from "../context/Context"
 
@@ -19,13 +19,13 @@ export function Header() {
                     <Row>
                         <Col className="d-flex align-items-center ms-3">
                             <a href="/cart" className="me-3 text-dark position-relative">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-basket" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-basket" viewBox="0 0 16 16" type="button" alt="link to cart">
                                 <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 13.5V9a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h1.217L5.07 1.243a.5.5 0 0 1 .686-.172zM2 9v4.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9H2zM1 7v1h14V7H1zm3 3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 6 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 8 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5z"/>
                                 </svg>
                                 {(numberOfItemsInCart > 0 
                                 ? <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
                                     {numberOfItemsInCart}
-                                    <span className="visually-hidden">unread messages</span>
+                                    <span className="visually-hidden">items in cart</span>
                                   </span>
                                 : null)}
                             </a>
