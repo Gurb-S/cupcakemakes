d5b2fb0dc73274bc4ea02ca7e0a660aeb8576f85
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-09-16 23:46:41 -0700

    center text

--- src/components/CustomOrder.js
+++ src/components/CustomOrder.js
@@ -1,7 +1,7 @@
 
 export function CustomOrder() {
     return(
-        <div>
+        <div className="text-center">
             <h1>Custom Order page</h1>
             <h2>Place your custom order here</h2>
         </div>
