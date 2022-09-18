833f212254680b485aa07a00965928ce157c87ea
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-09-17 23:16:07 -0700

    add from element

--- src/components/CustomOrder.js
+++ src/components/CustomOrder.js
@@ -4,6 +4,9 @@ export function CustomOrder() {
         <div className="text-center">
             <h1>Custom Order page</h1>
             <h2>Place your custom order here</h2>
+            <form>
+                <label>Description</label>
+            </form>
         </div>
         
     )
