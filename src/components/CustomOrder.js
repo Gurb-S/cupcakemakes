d860713fc5671ed833699a28c51195f2b8d6ba34
gurb <Organicmintz@gmail.com>
2022-09-19 19:54:19 -0700

    create initial custom form

--- src/components/CustomOrder.js
+++ src/components/CustomOrder.js
@@ -1,13 +1,22 @@
+import { Button } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
 
 export function CustomOrder() {
     return(
-        <div className="text-center">
-            <h1>Custom Order page</h1>
-            <h2>Place your custom order here</h2>
-            <form>
-                <label>Description</label>
-            </form>
-        </div>
-        
+        <Form className='text-center'>
+            <Form.Group className='m-auto mb-3 text-center w-75 p-3' controlId="formBasicName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type='text' placeholder='Enter your name' className='rounded-pill'/>
+            </Form.Group>
+            <Form.Group className='m-auto mb-3 w-75 p-3' controlId='formBasicNumber'>
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control type='tel' placeholder='(555) 555-5555' className='rounded-pill'/>
+            </Form.Group>
+            <Form.Group className='m-auto mb-3 w-75 p-3' controlId='formBasicDesc'>
+                <Form.Label>Descibe the style of cupcakes you would like</Form.Label>
+                <Form.Control as='textarea' rows={3} className='rounded-4'/>
+            </Form.Group>
+            <Button variant="primary" type="submit">Submit</Button>
+        </Form>
     )
 }
