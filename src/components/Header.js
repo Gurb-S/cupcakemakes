d51a9e00453abec96b1e54a4bcc474168d113fd6
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-09-23 22:40:57 -0700

    change cake to cupcake

--- src/components/Header.js
+++ src/components/Header.js
@@ -44,7 +44,7 @@ export function Header() {
                                 <div className="offcanvas-body">
                                     <Nav className="me-auto">
                                         <Nav.Link href="/">Home</Nav.Link>
-                                        <Nav.Link href="/custom">Custom Cake</Nav.Link>
+                                        <Nav.Link href="/custom">Custom Cupcake</Nav.Link>
                                         {/* <Nav.Link href="/">Cupcake Gallery</Nav.Link>
                                         <Nav.Link href="/">Cake Gallery</Nav.Link> */}
                                     </Nav>
