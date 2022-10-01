73d3b913f41c7aac0b566fa732bf1b34a92860e6
gurb <Organicmintz@gmail.com>
2022-09-30 22:04:41 -0700

    remove basic background

--- src/components/BodyGradient.js
+++ src/components/BodyGradient.js
@@ -2,7 +2,7 @@ import { Button } from "react-bootstrap"
 
 export function BodyGradient() {
     return(
-        <div className="p-5 bg-secondary bg-gradient text-white">
+        <div className="p-5 text-black main-gradient">
             <p>Welcome to Cupcake Makes, we're a business based in Plumas Lake, CA</p>
             <p>We offer both ordering predesigned cupcakes and making custom offers.</p>
             <div className="d-flex justify-content-center">
