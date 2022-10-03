0766b7546b059b857c4c0e38055492d375a7cfdb
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-10-02 23:48:33 -0700

    add text

--- src/components/BodyGradient.js
+++ src/components/BodyGradient.js
@@ -4,7 +4,7 @@ export function BodyGradient() {
     return(
         <div className="p-5 text-black main-gradient">
             <p>Welcome to Cupcake Makes, we're a business based in Plumas Lake, CA</p>
-            <p>We offer both ordering predesigned cupcakes and making custom offers.</p>
+            <p>We offer both ordering predesigned cupcakes and making custom offers. Keep it twisted</p>
             <div className="d-flex justify-content-center">
                 <Button variant="outline-dark" href="/custom" className="rounded-pill">Custom Order</Button>
             </div>
