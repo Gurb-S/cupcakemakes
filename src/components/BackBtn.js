3acdea18b21ac3abe3ed3632768fc74d1c7451fc
gurb <Organicmintz@gmail.com>
2022-10-04 14:10:36 -0700

    add back btn

--- src/components/BackBtn.js
+++ src/components/BackBtn.js
@@ -1,7 +1,11 @@
 import { Button } from "react-bootstrap";
+import back from "../imgs/icons/back.png";
 
 export function BackBtn() {
     return(
-        <Button variant="primary" className="d-lg-none" href="/">T</Button>
+        <Button variant="info" className="d-lg-none" href="/"><img src={back} width={40} height={40}/></Button>
     )
 }
+
+///run/media/gurb/64BE4FAE295985E0/github/cupcakemakes/src/imgs/icons/previous_medium.png
+//src/imgs/icons/previous_medium.png
