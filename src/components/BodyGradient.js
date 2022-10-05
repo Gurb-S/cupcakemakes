a35676071bb7ff3f48042d12d7a24de031e77ec5
gurb <Organicmintz@gmail.com>
2022-10-05 11:40:13 -0700

    add dark overlay

--- src/components/BodyGradient.js
+++ src/components/BodyGradient.js
@@ -2,11 +2,11 @@ import { Button } from "react-bootstrap"
 
 export function BodyGradient() {
     return(
-        <div className="p-5 text-black main-gradient">
+        <div className="p-5 text-info main-gradient">
             <p>Welcome to Cupcake Makes, we're a business based in Plumas Lake, CA</p>
-            <p>We offer both ordering predesigned cupcakes and making custom offers. Keep it twisted</p>
+            <p>We offer both ordering predesigned cupcakes and making custom offers.</p>
             <div className="d-flex justify-content-center">
-                <Button variant="outline-dark" href="/custom" className="rounded-pill">Custom Order</Button>
+                <Button variant="success" href="/custom" className="rounded-pill">Custom Order</Button>
             </div>
         </div>
     )
