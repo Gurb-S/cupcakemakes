c61239fbc51d6d078f4fe5159ca7eb170cfc19f0
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-10-08 21:45:07 -0700

    add description for back btn

--- src/components/BackBtn.js
+++ src/components/BackBtn.js
@@ -5,7 +5,7 @@ export function BackBtn(props) {
     console.log(props)
     return(
         <div className="mt-3 mx-2 d-flex justify-content-between">
-            <Button variant="info" className="d-lg-none my-auto" href="/"><img src={back} width={40} height={40}/></Button>
+            <Button variant="info" className="d-lg-none my-auto" href="/"><img src={back} width={40} height={40} alt="back button"/></Button>
             <h1 className="align-self-center">{props.page}</h1>
             <a href="/"><img src={"https://ik.imagekit.io/dod5kjssy/imgs/logo"} height={50} width={50} alt="logo" className="rounded-circle"></img></a>
         </div>
