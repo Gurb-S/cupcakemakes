01cb4bfde768e990e7333af566f52f9507ca4691
gurb <Organicmintz@gmail.com>
2022-10-13 13:47:20 -0700

    add styling for pwa header

--- src/components/BackBtn.js
+++ src/components/BackBtn.js
@@ -5,10 +5,10 @@ import back from "../imgs/icons/back.png";
 export function BackBtn(props) {
     console.log(props)
     return(
-        <div className="my-4 mx-2 d-flex justify-content-between web-mode pwa-mode">
-            <Button variant="info" className="d-lg-none my-auto bg-light border border-0" href="/"><img src={back} width={40} height={40} alt="back button"/></Button>
+        <div className="m-0 mb-5 pt-5 d-flex web-mode pwa-mode justify-content-between bg-primary">
+            <Button variant="info" className="d-lg-none my-auto ms-1 bg-primary border border-0" href="/"><img src={back} width={40} height={40} alt="back button"/></Button>
             <h1 className="align-self-center">{props.page}</h1>
-            <Link to={`/`}><img src={"https://ik.imagekit.io/dod5kjssy/imgs/logo"} height={50} width={50} alt="logo" className="rounded-circle"></img></Link>
+            <Link to={`/`} className="me-2"><img src={"https://ik.imagekit.io/dod5kjssy/imgs/logo"} height={50} width={50} alt="logo" className="rounded-circle"></img></Link>
         </div>
     )
 }
