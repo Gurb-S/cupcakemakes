d6aa9a178b7069fdc53825e44d2421fae23a2f28
gurb <Organicmintz@gmail.com>
2022-10-13 12:18:28 -0700

    give css file priority

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap'
-import './custom.css'
 import './custom.scss' 
+import './custom.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
