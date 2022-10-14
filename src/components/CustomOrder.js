d08b2bcf994a6e1ef365030e06a7968cc6401495
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-10-14 00:52:31 -0700

    add data so form can be read by netlify

--- src/components/CustomOrder.js
+++ src/components/CustomOrder.js
@@ -7,18 +7,25 @@ export function CustomOrder() {
         <div className='text-center'>
             <BackBtn page={"Custom Order"}/>
             <h1 className="my-4 no-header">Custom Order</h1>
-            <Form>
+            {/* <form name='contact' action="/success" method="POST">
+                <input type="hidden" name='form-name' value="contact" />
+                <input type='text' name='name' id='name' />
+                <input type="tel" name='phone' id='phone' placeholder='(555) 555-5555' />
+                <button type='submit'>Full SEND</button>
+            </form> */}
+            <Form name='Custom_Order' action="/success" method="POST">
+                <input type="hidden" name='form-name' value="Custom_Order" />
                 <Form.Group className='m-auto mb-3 text-center w-75 p-3' controlId="formBasicName">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control type='text' placeholder='Enter your name' className='rounded-pill'/>
+                    <Form.Control type='text' name='name' placeholder='Enter your name' className='rounded-pill'/>
                 </Form.Group>
                 <Form.Group className='m-auto mb-3 w-75 p-3' controlId='formBasicNumber'>
                     <Form.Label>Phone Number</Form.Label>
-                    <Form.Control type='tel' placeholder='(555) 555-5555' className='rounded-pill'/>
+                    <Form.Control type='tel' name='phone' placeholder='(555) 555-5555' className='rounded-pill'/>
                 </Form.Group>
                 <Form.Group className='m-auto mb-3 w-75 p-3' controlId='formBasicDesc'>
                     <Form.Label>Description</Form.Label>
-                    <Form.Control as='textarea' rows={3} placeholder='Describe the style of cupcakes you would like' style={{ height: '250px' }} className='rounded-4'/>
+                    <Form.Control as='textarea' rows={3} name='description' placeholder='Describe the style of cupcakes you would like' style={{ height: '250px' }} className='rounded-4'/>
                 </Form.Group>
                 <Button variant="primary" type="submit" className='rounded-pill px-4 mb-4'>Submit</Button>
             </Form>
