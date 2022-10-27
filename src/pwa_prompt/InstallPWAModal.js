df2f86374b8bce622d7424d83030d8dce634f330
gurb <Organicmintz@gmail.com>
2022-10-26 21:15:00 -0700

    create basic modal

--- src/pwa_prompt/InstallPWAModal.js
+++ src/pwa_prompt/InstallPWAModal.js
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import logo from "../imgs/icons/enlarge_all.png"
+import share from "../imgs/icons/share.png"
 
 
 export function InstallPWAModal(){
     const [smShow, setSmShow] = useState(true);
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
 
     return (
         <>
@@ -19,12 +22,15 @@ export function InstallPWAModal(){
                 aria-labelledby="example-modal-sizes-title-sm"
                 centered
             >
-                <Modal.Header closeButton={false}>
-                    <Modal.Title id="example-modal-sizes-title-sm" className="d-block">
-                        Install Cupcake Makes
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>...</Modal.Body>
+                <Modal.Body className="text-center">
+                    <img src={logo} height={50} width={50} alt="logo" className="rounded-circle"></img>
+                    <h1 className="fs-2 mt-2">Install Cupcake Makes</h1>
+                    <p className="text-muted">Install this application on your homescreen for a better experience</p>
+                    <p>Tap <img src={share} height={25} width={25} alt="apple share logo" className="mb-2"></img> then &quot;Add to Home Screen&quot;</p>
+                    <Button  onClick={handleClose} className="w-25 p-3 border border-0" style={{ color: "#5998F1"}}>
+                        Close
+                    </Button>
+                </Modal.Body>
             </Modal>
       </>
     )
