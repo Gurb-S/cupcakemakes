3c6c075626b0f51bb5955d5d91d57f6e1925cdb9
gurb <Organicmintz@gmail.com>
2022-10-28 14:24:46 -0700

    change text

--- src/components/BodyGradient.js
+++ src/components/BodyGradient.js
@@ -9,7 +9,7 @@ export function BodyGradient() {
     return(
         <div className={isInWebAppiOS || isInWebAppChrome ? "p-5 mt-5 text-info main-gradient" : "p-5 text-info main-gradient"}>
             <p className={isInWebAppiOS || isInWebAppChrome ? "mt-5" : ''}>Welcome to Cupcake Makes, we're a business based in Plumas Lake, CA</p>
-            <p>We offer both ordering predesigned cupcakes and placing custom orders.</p>
+            <p>We offer both ordering predesigned cupcakes and placing custom orders!</p>
             <div className="d-flex justify-content-center">
                 <Button variant="success" className="rounded-pill"><Link to={`/custom`} className="text-decoration-none text-danger">Custom Order</Link></Button>
             </div>
