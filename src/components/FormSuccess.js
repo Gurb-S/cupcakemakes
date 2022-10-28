edfe0eacd735dd203525b152b24ab6ff79d8f2aa
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-10-28 01:57:06 -0700

    change text of thanks screen

--- src/components/FormSuccess.js
+++ src/components/FormSuccess.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export function FormSuccess(){
     return(
         <div className="text-center mt-5">
-            <h1 className="fs-2">Form has been submitted!!</h1>
+            <h1 className="fs-2">Order has been submitted!!</h1>
             <Link to={`/`}>Click here to go back home</Link>
         </div>
     )
