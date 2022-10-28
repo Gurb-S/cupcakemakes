6e605a9fbb272ada25e8c1e2a80b6cc45c4dfc07
gurb <Organicmintz@gmail.com>
2022-10-28 14:29:03 -0700

    add to success form

--- src/components/FormSuccess.js
+++ src/components/FormSuccess.js
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom"
 export function FormSuccess(){
     return(
         <div className="text-center mt-5">
-            <h1 className="fs-2">Order has been submitted!!</h1>
+            <h1 className="fs-2 mb-5">Order has been submitted🎉</h1>
+            <h2 className="fs-4 mb-5">You will get a confirmation email soon!</h2>
             <Link to={`/`}>Click here to go back home</Link>
         </div>
     )
