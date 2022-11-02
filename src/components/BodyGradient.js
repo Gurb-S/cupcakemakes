80044a2e2b2786d3b7aeed9bc6c81eace6ebc1dc
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-11-01 23:40:54 -0700

    add todo list

--- src/components/BodyGradient.js
+++ src/components/BodyGradient.js
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom"
 
 export function BodyGradient() {
 
+    // TODO: make gradient taller in desktop mode (lg)
+
     let isInWebAppiOS = (window.navigator.standalone === true);
     let isInWebAppChrome = (window.matchMedia('(display-mode: standalone)').matches);
 
     return(
         <div className={isInWebAppiOS || isInWebAppChrome ? "p-5 mt-5 text-info main-gradient" : "p-5 text-info main-gradient"}>
-            <p className={isInWebAppiOS || isInWebAppChrome ? "mt-5" : ''}>Welcome to Cupcake Makes, we're a business based in Plumas Lake, CA</p>
+            <p className={isInWebAppiOS || isInWebAppChrome ? "mt-5" : 'text-center'}>Welcome to Cupcake Makes, we're a business based in Plumas Lake, CA</p>
             <p className="text-center">We offer both ordering predesigned cupcakes and placing custom orders!</p>
             <div className="d-flex justify-content-center">
                 <Button variant="success" className="rounded-pill"><Link to={`/custom`} className="text-decoration-none text-danger">Custom Order</Link></Button>
