4bd6cb5082f32f7273c432841332f40acd5a8ac7
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-11-04 23:43:37 -0700

    remove unnecessary code

--- src/components/Body.js
+++ src/components/Body.js
@@ -12,9 +12,7 @@ export function Body() {
     const cupcakes = products.cupcakes
     //const cakes = products.cakes
 
-    const { prompt, daysBetween }  = useIsIOS();
-    console.log(daysBetween)
-    // console.log(browserName)
+    const { prompt }  = useIsIOS();
 
     const cupcakeCards = cupcakes.map((card) => {
         return <ProductCard 
@@ -33,8 +31,6 @@ export function Body() {
     //     />
     // })
     // * end creating cupcake
-    // const [modalShow, setModalShow] = React.useState(false);
-    //console.log(prompt)
     return(
         <>
             <div className="m-0 pt-5 pb-2 d-flex web-mode pwa-mode justify-content-center bg-primary fixed-top">
@@ -42,15 +38,6 @@ export function Body() {
             </div>
             <BodyGradient />
             { prompt && <InstallPWAModal /> }
-            {/* <Button variant="primary" onClick={() => setModalShow(true)}>
-                Launch vertically centered modal
-            </Button>
-
-            <InstallPWAModal
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-            /> */}
-            {/* <InstallPWAModal /> */}
             <div className="text-center">
                 <h1 className="mt-3 fs-1">
                     CUPCAKES
