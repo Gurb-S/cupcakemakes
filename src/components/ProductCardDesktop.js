bfb701df3bf3c5c840146e2f41af3765497d36b7
gurb <Organicmintz@gmail.com>
2022-11-08 14:43:55 -0800

    remove comments

--- src/components/ProductCardDesktop.js
+++ src/components/ProductCardDesktop.js
@@ -2,12 +2,14 @@ import Card from 'react-bootstrap/Card';
 import { Link } from "react-router-dom";
 
 export function ProductCardDesktop(props) {
+
+    // style={{background: "linear-gradient(rgba(0, 0, 0, 0.678), rgba(0, 0, 0, 0.75))"}}
     return (
         <Card className="bg-light text-white my-2">
             <Link to={`products/${props.id}`} className="text-white text-decoration-none">
                 <Card.Img src={`${props.img}`} width={250} height={250} alt={`${props.name} Img`} />
-                <Card.ImgOverlay className="overlay-gradient">
-                <Card.Title className="fs-2">{props.name}</Card.Title>
+                <Card.ImgOverlay className='overlay-gradient'>
+                    <Card.Title className="fs-2">{props.name}</Card.Title>
                 </Card.ImgOverlay>
             </Link>
         </Card>
