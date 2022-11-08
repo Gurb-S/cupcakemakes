7f93929ab88a64f878ceffe9c0d7b3930ecde056
gurb <Organicmintz@gmail.com>
2022-11-08 13:27:11 -0800

    add product card for desktop

--- src/components/ProductCardDesktop.js
+++ src/components/ProductCardDesktop.js
@@ -1,10 +1,15 @@
-import { Card, Col } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import { Link } from "react-router-dom";
 
-export function ProductCardDesktop() {
+export function ProductCardDesktop(props) {
     return (
-        <div>
-            Display format for desktop layout
-        </div>
+        <Card className="bg-light text-white my-2">
+            <Link to={`products/${props.id}`} className="text-white text-decoration-none">
+                <Card.Img src={`${props.img}`} width={250} height={250} alt={`${props.name} Img`} />
+                <Card.ImgOverlay className="overlay-gradient">
+                <Card.Title className="fs-2">{props.name}</Card.Title>
+                </Card.ImgOverlay>
+            </Link>
+        </Card>
     )
 }
