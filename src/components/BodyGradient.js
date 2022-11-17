c35538c50ff9cf00c78e091fa7eff1f85672a41a
gurb <Organicmintz@gmail.com>
2022-11-16 19:45:05 -0800

    make gradient large based on screen size

--- src/components/BodyGradient.js
+++ src/components/BodyGradient.js
@@ -9,13 +9,12 @@ export function BodyGradient() {
     let isInWebAppChrome = (window.matchMedia('(display-mode: standalone)').matches);
 
     return(
-        <div className={isInWebAppiOS || isInWebAppChrome ? "p-5 mt-5 text-info main-gradient" : "p-5 text-info main-gradient"}>
-            <p className={isInWebAppiOS || isInWebAppChrome ? "mt-5" : 'text-center'}>Welcome to Cupcake Makes, we're a business based in Plumas Lake, CA</p>
+        <div className={isInWebAppiOS || isInWebAppChrome ? "p-5 mt-5 text-info main-gradient" : "p-5 text-info main-gradient large"}>
+            <p className={isInWebAppiOS || isInWebAppChrome ? "mt-5" : 'text-center mt-5 pt-5'}>Welcome to Cupcake Makes, we're a business based in Plumas Lake, CA</p>
             <p className="text-center">We offer both ordering predesigned cupcakes and placing custom orders!</p>
             <div className="d-flex justify-content-center">
                 <Button variant="success" className="rounded-pill"><Link to={`/custom`} className="text-decoration-none text-danger">Custom Order</Link></Button>
             </div>
         </div>
     )
-
 }
