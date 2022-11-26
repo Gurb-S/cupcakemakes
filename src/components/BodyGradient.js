94bb15b80fc8142220a60aa0c5e3ab5a1b94796a
gurb <Organicmintz@gmail.com>
2022-11-26 15:09:36 -0800

    add css classes for text

--- src/components/BodyGradient.js
+++ src/components/BodyGradient.js
@@ -10,8 +10,8 @@ export function BodyGradient() {
 
     return(
         <div className={isInWebAppiOS || isInWebAppChrome ? "p-5 mt-5 text-info main-gradient" : "p-5 text-info main-gradient large"}>
-            <p className={isInWebAppiOS || isInWebAppChrome ? "mt-5" : 'text-center mt-5 pt-5'}>Welcome to Cupcake Makes, we're a business based in Plumas Lake, CA</p>
-            <p className="text-center">We offer both ordering predesigned cupcakes and placing custom orders!</p>
+            <p className={isInWebAppiOS || isInWebAppChrome ? "mt-5" : 'text-center large-screen'}>Welcome to Cupcake Makes, we're a business based in Plumas Lake, CA</p>
+            <p className="text-center large-text">We offer both ordering predesigned cupcakes and placing custom orders!</p>
             <div className="d-flex justify-content-center">
                 <Button variant="success" className="rounded-pill"><Link to={`/custom`} className="text-decoration-none text-danger">Custom Order</Link></Button>
             </div>
